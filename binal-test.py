ff2bb2a003f4389b367c398bf8899e459251f8d4
Binal Kamani <bikamani@microsoft.com>
2019-07-23 00:51:00 +0000

    user config

---
+++ binal-test.py
@@ -0,0 +1,14 @@
+import djisdkpy
+import time
+
+drone = djisdkpy.Drone('UserConfig.txt')
+drone.initialize()
+time.sleep(1)
+drone.takeControl()
+drone.arm()
+drone.takeoff()
+time.sleep(5)
+drone.land()
+drone.disarm()
+drone.releaseControl()
+drone.shutdown(
